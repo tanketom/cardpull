2d6039872ea6522d543d264b29c8668f43bf1c82
@Tanketom <tanketom@gmail.com>
2024-09-25 20:25:53 +0200

    test

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-document.getElementById('deck').addEventListener('click', pullCard);
+document.addEventListener('DOMContentLoaded', setupDeck);
 
-async function pullCard() {
+async function setupDeck() {
     const response = await fetch('cards.json');
     const data = await response.json();
     const cards = data.cards;
-    const randomCard = cards[Math.floor(Math.random() * cards.length)];
-    displayCard(randomCard);
+    const deckContainer = document.getElementById('deck-container');
+
+    cards.forEach(card => {
+        const cardElement = document.createElement('img');
+        cardElement.src = data.backside;
+        cardElement.className = 'deck-card';
+        cardElement.dataset.card = JSON.stringify(card);
+        deckContainer.appendChild(cardElement);
+    });
+
+    document.querySelectorAll('.deck-card').forEach(card => {
+        card.addEventListener('click', pullCard);
+    });
+}
+
+function pullCard(event) {
+    const card = JSON.parse(event.target.dataset.card);
+    displayCard(card);
+    event.target.remove();
 }
 
 function displayCard(card) {
